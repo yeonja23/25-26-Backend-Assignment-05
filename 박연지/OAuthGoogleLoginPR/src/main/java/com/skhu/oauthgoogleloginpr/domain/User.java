20f7f02e5c51b022c44d223203f4b25cf999ea3d
Park, Yeonji <yeonpyj23@gmail.com>
2025-11-22 18:54:30 +0900

    refactor: User 엔티티 불필요한 양방향 매핑 제거

--- 박연지/OAuthGoogleLoginPR/src/main/java/com/skhu/oauthgoogleloginpr/domain/User.java
+++ 박연지/OAuthGoogleLoginPR/src/main/java/com/skhu/oauthgoogleloginpr/domain/User.java
@@ -1,20 +1,15 @@
 package com.skhu.oauthgoogleloginpr.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -31,9 +26,6 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Post> posts = new ArrayList<>();
 
     @Builder
     public User(String username, String password, String email, Role role) {
